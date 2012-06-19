0b5924ea7e34ca91db58e4aa0aabb0b886ed26e7
mazza <mazza@bee1b3ed-c3eb-0310-9994-b88e04532788>
2012-06-19 16:45:44 +0000

    abstraido DifferentTestCase
    base para testes com arquivos gravados para diffs
    
    git-svn-id: http://repositorio.interlegis.leg.br/PortalInterlegis/produtos/il.portalinterlegis/trunk@6534 bee1b3ed-c3eb-0310-9994-b88e04532788

---
+++ src/il/portalinterlegis/tests/differenttestcase.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import unittest2 as unittest
+from itertools import count
+import os
+
+
+class DifferentTestCase(unittest.TestCase):
+    """ Test case with differences output to temporary files
+    """
+
+    def __init__(self, *args):
+        try:
+            self.diff_counter = count(1)
+            while True:
+                f1, f2 = self._next_out_filenames()
+                os.remove(f1)
+                os.remove(f2)
+        except OSError:
+            pass
+        self.diff_counter = count(1)
+        super(DifferentTestCase, self).__init__(*args)
+
+    def _next_out_filenames(self):
+        base = "out_%s_%s" % (self.__class__.__name__, self.diff_counter.next())
+        return ("%s.%s~" % (base, suffix) for suffix in ['first', 'second'])
+
+    def assertMultiLineEqual(self, first, second, *args):
+        """Same as standard assertMultiLineEqual,
+        but outputs differing arguments to files names
+        out_<couter>.1~ and
+        out_<couter>.2~ where counter is an incrementing one."""
+
+        if first is not None and second is not None and first != second:
+            for fname, content in zip(self._next_out_filenames(), [first, second]):
+                with open(fname, "w+") as f:
+                    f.write(content)
+        super(DifferentTestCase, self).assertMultiLineEqual(first, second, *args)
+
+
