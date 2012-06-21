a4e40376a9b797ea79e96433358c28b49118d29c
mazza <mazza@bee1b3ed-c3eb-0310-9994-b88e04532788>
2012-06-21 03:47:30 +0000

    collective.js.jqueryui configurado
    git-svn-id: http://repositorio.interlegis.leg.br/PortalInterlegis/produtos/il.portalinterlegis/trunk@6539 bee1b3ed-c3eb-0310-9994-b88e04532788

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(name='il.portalinterlegis',
           'plone.formwidget.contenttree',
           'plone.formwidget.autocomplete',
           'Jinja2', # This one is not neurotic about missing values. We need this.
+          'collective.js.jqueryui',
       ],
       extras_require={'test': ['plone.app.testing', 'mock']},
       entry_points="""
