25f236cbcc01d17f7385e5722120c96420bb7a6c
mazza <mazza@bee1b3ed-c3eb-0310-9994-b88e04532788>
2012-06-27 15:13:57 +0000

    Abas dinamicas
    git-svn-id: http://repositorio.interlegis.leg.br/PortalInterlegis/produtos/il.portalinterlegis/trunk@6559 bee1b3ed-c3eb-0310-9994-b88e04532788

--- src/il/portalinterlegis/browser/boxes/tabs.py
+++ src/il/portalinterlegis/browser/boxes/tabs.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
-from manager import get_template
+from manager import get_template, Row
 
 
-class Tab(object):
+class Tab(Row):
+
+    template_name = 'tab.html'
 
     def __init__(self, title, inner_title, *row_spec):
         self.title = title
         self.inner_title = inner_title
         self.row_spec = row_spec
+        super(Tab, self).__init__(*row_spec)
 
     def __call__(self, context):
-        return 'TODO: TAB %s' % self.title
+        return self.render(context)
+
+    def render(self, context):
+        return get_template(self.template_name).render(
+            cells=self.cells(context),
+            title=self.title,
+            inner_title=self.inner_title)
 
 
 class TabbedPane(object):
@@ -21,4 +30,5 @@ class TabbedPane(object):
     def __call__(self, context):
         template = get_template("tabbedpane.html")
         return template.render(
-            tabs=[(tab.title, tab(context)) for tab in self.tabs])
+            titles=[tab.title for tab in self.tabs],
+            tabs=[tab(context) for tab in self.tabs],)
