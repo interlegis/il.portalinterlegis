a374b8c97737b75527a413b904629ab30adda2bd
Marcio Mazza <mazza@interlegis.leg.br>
2012-07-10 14:04:20 +0000

    botoes next previous acertados no carrossel Produtos e Servicos

--- src/il/portalinterlegis/static/js/carousel.js
+++ src/il/portalinterlegis/static/js/carousel.js
@@ -8,5 +8,13 @@ $(function() {
                    autoSlideInterval: 5000});
     $("div.products-services")
         .carousel({pagination: false,
+                   nextBtnInsertFn: function (elts) {
+                       var buttons = elts.carousel.parent().parent().children('.next-previous-buttons');
+                       return buttons.children('input[value="Next"]')
+                   },
+                   prevBtnInsertFn: function (elts) {
+                       var buttons = elts.carousel.parent().parent().children('.next-previous-buttons');
+                       return buttons.children('input[value="Previous"]')
+                   },
                    loop: true});
 });
