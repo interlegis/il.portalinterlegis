767ef43d142385de41015f1145cf838cecece2a6
Thiago Cesar Bernadino <thiagobernadino@interlegis.leg.br>
2012-08-31 20:50:06 +0000

    Atulizacao bloco noticia

--- src/il/portalinterlegis/browser/boxes/__init__.py
+++ src/il/portalinterlegis/browser/boxes/__init__.py
@@ -10,13 +10,8 @@ class LastNews(object):
     def __call__(self, context):
         template = get_template("lastnews.html")
         # TODO: fazer busca no catalogo por tag
-        news = [
-            (u"UVESC reafirma interesse em ampliar parceria com Interlegis", "news/TODO"),
-            (u"Parlamento do Timor Leste quer apoio do Interlegis para se organizar", "news/TODO"),
-            (u"Itaperuna deve sediar oficina de Portal Modelo para 15 cidades", "news/TODO"),
-            (u"UVESC reafirma interesse em ampliar parceria com Interlegis", "news/TODO"),
-            (u"Parlamento do Timor Leste quer apoio do Interlegis para se organizar", "news/TODO"),
-            ]
+        busca = context.portal_catalog(portal_type="News Item")[:5]
+        news = [(noticia.getObject().title, noticia.getURL()) for noticia in busca]
         # TODO: traduzir kind de tag para class css
         return template.render(news=news, css_class=self.kind)
 
