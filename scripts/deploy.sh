c961b4d1c37ea7a719fa0603f0b6b0d635a754a8
Marcio Mazza <mazza@interlegis.leg.br>
2013-05-02 10:41:42 -0300

    script de deploy expandido para enviar todos os ramos

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-
 # atualiza tag "release" para apontar para a revisao corrente
-git tag -d release && git push origin :release && git tag release && git push && git push --tags
+
+# deleta a tag release aqui e remotamente
+git tag -d release
+git push origin :release
+
+# recria a tag release e envia tudo
+git tag release
+git push
+git push --all
+git push --tags
