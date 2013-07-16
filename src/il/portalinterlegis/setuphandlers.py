288a22e7fff730eb7adae3c30b5c5a205ebe81cc
Marcio Mazza <mazza@interlegis.leg.br>
2013-07-15 21:06:04 -0300

    limpeza de codigo nao usado
    Conflicts:
    	buildout.cfg

--- src/il/portalinterlegis/setuphandlers.py
+++ src/il/portalinterlegis/setuphandlers.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-from zope.component import getUtility
-from plone.i18n.normalizer.interfaces import IIDNormalizer
 from content.createcontent import createObjects
-from browser.homes import IComunidadeLegislativa, IInformacao, ICapacitacao, ITecnologia, IComunicacao
 
 
 def setupVarious(context):
@@ -22,30 +19,8 @@ def setupVarious(context):
         del portal['front-page']
     portal.setLayout('home')
 
-    # navegacao de 1o nivel
-    normalizer = getUtility(IIDNormalizer)
-    folders_do_menu = [{'id': normalizer.normalize(title),
-                     'title': title,
-                     'description': u'Seção %s' % title,
-                     'type': 'Folder',
-                     'workflow_transition': 'publish',
-                     'exclude_from_nav': False,
-                     'layout': 'home',
-                     'marker_interface': marker}
-                     for title, marker in [
-                             (u'Comunidade Legislativa', IComunidadeLegislativa),
-                             (u'Informação', IInformacao),
-                             (u'Capacitação', ICapacitacao),
-                             (u'Tecnologia', ITecnologia),
-                             (u'Comunicação', IComunicacao),
-                             ]]
-    createObjects(portal, folders_do_menu)
-
-    # esconde todo o resto
-    ids_folders_do_menu = [f['id'] for f in folders_do_menu] + ['front-page']
-    for id in portal.objectIds():
-        if id not in ids_folders_do_menu:
-            obj = portal[id]
-            if hasattr(obj, 'exclude_from_nav'):
-                obj.setExcludeFromNav(True)
-                obj.reindexObject()  # nao sei exatamente pq isso, mas nao custa colocar
+    # cria objetos obrigatorios
+    createObjects(portal, [{'id': 'interlegisnamidia',
+                            'title': u'Interlegis na Mídia',
+                            'description': u'Notícias e novidades sobre o Programa Interlegis na Mídia',
+                            'type': 'Folder',}])
