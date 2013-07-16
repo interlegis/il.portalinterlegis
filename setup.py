b38f6eecb2d650459d0d06e01ffe7a07ffb9553b
Marcio Mazza <mazza@interlegis.leg.br>
2013-07-15 21:33:44 -0300

    instalado plonetheme.bootstrap
    Conflicts:
    	buildout.cfg
    	setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(name='il.portalinterlegis',
           'psycopg2', # for ZPsycopgDA, that must be installed as an old style product
           'sc.social.like',
           'Products.PloneFormGen',
+          'plonetheme.bootstrap',
       ],
       extras_require={'test': ['plone.app.testing', 'mock']},
       entry_points="""
